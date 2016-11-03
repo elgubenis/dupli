cae65403c386bf13cec88a5c6cf81afe7d3008d9
Linus Gubenis <elgubenis@gmail.com>
2016-11-02 20:42:53 -0600

    done

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # dupli
 get the next duplicate (file-)name providing some kind of simple naming pattern and existing entries + counter
+
+## install
+```sh
+$ npm install dupli --save
+```
+
+## usage
+dupli(String, Array, String);
+```js
+const newName = dupli('copy 1 new document', [], 'copy');
+// newName: copy 2 new document
+
+const newName = dupli('new document', ['copy 2 new document', 'copy 1 new document'], 'copy');
+// newName: copy 3 new document
+```
+
+## note
+please note, the array of files is only used to get the highest number, the string which follows copy 2 for example is not taken in account to match the original filename passed as the first argument.
+You could as well just pass copy 2, copy 3, the idea here is that the array is an array of filenames which are already duplicates of the same file which gets duplicated using dupli.
+
+Thats due to the necesity of this module, if you need it to be taken into account just fork and add an option and merge back if you like.
--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-const copier = (originalName = '', counterNames = [], prefix = '') => {
+'use strict';
+
+const dupli = (originalName, counterNames, prefix) => {
   // get the originalName into a local variable
-  let name = originalName;
+  let name = originalName || '';
 
   // set a default highest
   let highest = 1;
 
-  // go through all the other files which have to do with the counting of the original one
+  counterNames = counterNames || [];
+  prefix = prefix || '';
+
+  // go through all the other files which impact the original 'highest'
   counterNames.forEach(counterName => {
-    // split name by prefix + whitespace, get text following
+    // split name by prefix + whitespace, get the string which follows
     const prefixSplit = counterName.split(`${prefix} `)[1];
 
     // if no text, return, this file does not take part in counting
     if (!prefixSplit) return;
 
-    // get first string between next whitespace (and after prefix)
+    // get first string between next whitespace (and after prefix) and try to turn it into a number
     const firstChar = Number(prefixSplit.split(' ').shift());
 
     // if its not a number, this file does not take part in counting
     if (isNaN(firstChar)) return;
 
-    // if the number is higher than the current highest, replace with + 1
-    if (firstChar > highest) highest = firstChar + 1;
+    // if the number is higher than the current highest, replace with this number + 1
+    if (firstChar >= highest) highest = firstChar + 1;
   });
 
-  // name already contains prefix + a number
+  // see if the name already contains the pattern (is a duplicate already)
   const matches = name.match(`^(?:${prefix}(?=\\s[0-9]))\\s\\d`);
-  if (matches) {
 
+  // is a duplicate, replace the pattern portion of the name
+  if (matches) {
     // replace the prefix + whitespace + number with a new number
     name = name.replace(new RegExp(`^(?:${prefix}(?=\\s[0-9]))\\s\\d`), `${prefix} ${highest}`);
   } else {
-
     // set the name to the prefix + name
     name = `${prefix} ${highest} ${name}`;
   }
@@ -38,4 +43,4 @@ const copier = (originalName = '', counterNames = [], prefix = '') => {
   return name;
 };
 
-module.exports = copier;
+module.exports = dupli;
